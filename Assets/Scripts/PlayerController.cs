aae696971db3355539fd74eff55a752a6121f40d
thejoryle <thejoryle@gmail.com>
2024-03-06 13:56:30 -0800

    added not working jump and player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody rb;
+    public float moveSpeed;
+    public float jumpForce;
+    public float gravity;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector3(0, gravity, moveSpeed);
+    }
+
+    void OnJump()
+    {
+        rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
     }
 }
